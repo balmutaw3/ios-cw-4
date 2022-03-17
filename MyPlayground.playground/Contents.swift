5c0fb45dbcdeef12d4f8da0a70668b9d7eef7add
balmutaw3 <100786689+balmutaw3@users.noreply.github.com>
2022-03-17 21:24:46 +0300

    Done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var name =    "Bader "
+var favoritenumber = 17
+var hobby = "soccer"
+print("Hello my name is \(name) and my favorie number is \(favoritenumber) and my favorite hobby is \(hobby) ")
